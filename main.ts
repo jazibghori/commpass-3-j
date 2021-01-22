084422e32ccb05e5fd2ffd41eef9f4be378bbb86
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-22 17:12:41 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,9 @@ basic.forever(function () {
     bearing = input.compassHeading()
     if (bearing < 45 || bearing > 315) {
         basic.showString("N")
+    } else if (bearing < 225 && bearing > 135) {
+        basic.showString("S")
     } else {
-        basic.showString("")
+        basic.showString("x")
     }
 })
